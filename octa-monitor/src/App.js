f5620d7a64f6d89823b1e366179835ce8be1bf79
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2021-12-22 01:39:17 +0530

    stopped react app component

--- octa-monitor/src/App.js
+++ octa-monitor/src/App.js
@@ -4,9 +4,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <video id="video" width="640" height="480" autoplay></video>
-        <button id="snap">Snap Photo</button>
-        <canvas id="canvas" width="640" height="480"></canvas>
       </div>
     );
   }
